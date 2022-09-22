a69a0e3d5e4cd0f742d41a797aa21205e8967a47
Heber Herraz <heber.herraz@trebol-it.com>
2022-09-22 16:33:49 -0300

    mas cambios

--- src/pages/general/NotFound.tsx
+++ src/pages/general/NotFound.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { PaperClipIcon } from '@heroicons/react/20/solid'
+import scarecrow from "../../assets/Scarecrow.png";
 
 const NotFound = () => {
   return (
-    <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg font-medium leading-6 text-gray-900">404 NOT FOUND</h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+    <div className="m-2">
+      <h3 className="text-lg font-medium leading-6 text-gray-900 p-3">
+        404 NOT FOUND
+      </h3>
+      <div className="p-5 md:m-24 md:flex">
+        <div className="md:flex-initial">
+          <img className="" src={scarecrow} alt="ScareCrow" />
+        </div>
+        <div className="md:flex-initial md:pt-2.5 pt-4">
+          <h2 className="text-6xl font-medium leading-9 md:tracking-wide md:mt-24">
+            I have bad news for you
+          </h2>
+          <p className="md:pt-10 md:pb-10 pt-20">
+            The page you are looking for might be removed or is temporarily
+            unavailable
+          </p>
+          <div className="bg-gray-600 text-lg text-white md:w-52 md:h-auto">
+            <p className="mt-20 text-center">BACK TO THE HOMEPAGE</p>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export {NotFound}
+export { NotFound };
